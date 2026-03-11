f00a481e4f6f4a8c6f22e4d3b042b83557e52149
Анастасия <boskoridzi@gmail.com>
2026-03-11 13:40:49 +0300

    Add files via upload

---
+++ domashnee_zadanie2.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+
+int main()
+
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+	std::string text;
+	std::cout << "Введите слово : " << std::endl;
+	std::getline(std::cin, text);
+	std::cout << "Вы ввели : " << std::endl << text << std::endl;
+
+	return 0;
+}
+
+
